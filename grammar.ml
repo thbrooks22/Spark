f86dcf4ca0a59e5cac28d3e3ccb91cbf65105c71
Thomas Brooks <thomasbrooks@Thomass-MBP-6.lan>
2020-04-10 17:59:47 -0400

    finished rudimentary typecheck, some cleanup

--- grammar.ml
+++ grammar.ml
@@ -3,6 +3,8 @@ open Grammar
 
 exception UnboundVarException of string
 exception UnboundTypeVarException of string
+exception ApplicationException of string
+exception TypeApplicationException of string
 exception TypeException of string
 
 module rec Type : Grammar.Type
@@ -23,31 +25,43 @@ module rec Type : Grammar.Type
     (* type variable for parametric polymorphism*)
     | Typevar of string =
   struct
-    module Gamma =
-      struct
-        type t = string * tp
-        let compare a b = String.compare (fst a) (fst b)
-      end ;;
-
-    (*
+    (* *************************************************************************
       GammaSet : A typing environment. Maps bound program variables to their types.
         Implemented as a set of string * tp tuples.
-    *)
-    module GammaSet = Set.Make(Gamma) ;;
+    ************************************************************************* *)
+    module GammaSet = Set.Make(
+                              struct
+                                type t = string * tp
+                                let compare a b = String.compare (fst a) (fst b)
+                              end
+                      ) ;;
     type gammaset = GammaSet.t ;;
+
     let empty_type_context = GammaSet.empty ;;
+
     (* type_of : Method to find the type of variable x in context gamma. *)
     let type_of (gamma : gammaset) (x : string) : tp option =
       try Some (snd (GammaSet.find (x, Unit) gamma))
       with Not_found -> None ;;
 
-    (*
+    (* append : Method to expand/edit type context with variable x of type tau. *)
+    let append (gamma : gammaset) (x : string) (tau : tp): gammaset =
+      let gamma' = GammaSet.remove (x, tau) gamma in
+      GammaSet.add (x, tau) gamma' ;;
+
+    (* *************************************************************************
+    ************************************************************************* *)
+
+
+    (* ************************************************************************
       DeltaSet : A set of bound type variables.
-    *)
+    ************************************************************************* *)
     module DeltaSet = Set.Make(String) ;;
     type deltaset = DeltaSet.t ;;
+
     let empty_typevar_set = GammaSet.empty ;;
 
+    let add = DeltaSet.add ;;
 
     let rec ok (tau : tp) (delta : deltaset) : bool =
       match tp with
@@ -63,15 +77,56 @@ module rec Type : Grammar.Type
       | Sum (tau1, tau2) ->
         ok tau1 delta && ok tau2 delta
       | Forall (x, tau') ->
-        ok tau' (DeltaSet.add x delta)
+        ok tau' (add x delta)
       | Typevar x -> DeltaSet.mem x delta ;;
+
+    (* *************************************************************************
+    ************************************************************************* *)
+
+
+    (*
+      subtype : Method to decide whether tau1 is a subtype of tau2 (currently
+        just implemented as equality, will expand later).
+    *)
+    let subtype (tau1 : tp) (tau2 : tp) : bool =
+      tau1 == tau2 ;;
+
+
+    (*
+      type_subst : Method to substitute a type variable according to
+        tau1{tau2 / x}.
+    *)
+    let rec type_subst (tau1 : tp) (x : string) (tau2 : tp) : tp =
+      match tau1 with
+      | Unit
+      | Int
+      | Bool
+      | String -> tau1
+      | Record lst ->
+        Record (List.map (fun (str, tau) -> (str, type_subst tau x tau2)) lst)
+      | To (tau, tau') ->
+        To (type_subst tau x tau2, type_subst tau' x tau2)
+      | Sum (tau, tau') ->
+        Sum (type_subst tau x tau2, type_subst tau' x tau2)
+      | Forall (x', tau) ->
+        if x' == x then tau1
+        else Forall (x', type_subst tau x tau2)
+      | Typevar x' ->
+        if x' == x then tau2
+        else tau1 ;;
+
 
 
     let new_type_var (delta : deltaset) : string =
-      let rec new_type_var' (delta' : deltaset) (str : string) : string =
-        if ok str delta'
-          then new_type_var' delta' (str ^ "0")
-        else str
+      let str = "X" in
+      let suff = ref 0 in
+      let str' = ref str in
+      let rec new_type_var' (delta' : deltaset) : string =
+        if ok !str' delta'
+          then suff := !suff + 1;
+            str' := str ^ string_of_int !suff;
+            new_type_var' delta' !str'
+        else !str'
         in
       new_type_var' delta "X" ;;
 
@@ -83,27 +138,53 @@ module rec Type : Grammar.Type
         (match type_of gamma x with
         | Some tau ->
           if ok tau delta then tau
-          else raise (UnboundTypeVarException "Type variable binding error.")
+          else raise (UnboundTypeVarException "Unbound type variable.")
         | None -> raise (UnboundVarException "Variable " ^ x ^ " unbound."))
       | Int _ -> Int
       | Neg e' ->
-        if (type_check gamma delta e') == Int then Int
+        if subtype (type_check gamma delta e') Int then Int
         else raise (TypeException "Type Int expected.")
       | Plus (e1, e2)
       | Times (e1, e2)
       | Pow (e1, e2) ->
-        if (type_check gamma delta e1) == Int && (type_check gamma delta e2) == Int
+        if subtype (type_check gamma delta e1) Int && subtype (type_check gamma delta e2) Int
           then Int
         else raise (TypeException "Type Int expected.")
       | Bool _ -> Bool
       | Equals (e1, e2)
       | Lessthan (e1, e2) ->
-        if (type_check gamma delta e1) == Int && (type_check gamma delta e2) == Int
+        if subtype (type_check gamma delta e1) Int && subtype (type_check gamma delta e2) Int
           then Bool
         else raise (TypeException "Type Int expected.")
-      | Lam ((x, tau), e) ->
-
-
+      | Lam ((x, tau), e') ->
+        let tau' = type_check (append gamma x tau) delta e' in
+        if ok tau delta then To (tau, tau')
+        else raise (UnboundTypeVarException "Unbound type variable.")
+      | App (e1, e2) ->
+        try
+          let To (tau, tau') = type_check gamma delta e1 in
+          let tau'' = type_check gamma delta e2 in
+          if subtype tau'' tau then tau'
+          else raise (ApplicationException "Type of argument does not match function signature.")
+        with Match_failure ->
+          raise (ApplicationException "Attempted to apply non-function.")
+      | Let ((x, tau), e1, e2) ->
+        if not (ok tau delta) then raise (UnboundTypeVarException "Unbound type variable.")
+        else
+          if subtype (type_check gamma delta e1) tau then
+            type_check (append gamma x tau) delta e2
+          else raise (TypeException "Type declaration does not match expression.")
+      | Typelam (x, e') ->
+        let tau = type_check gamma (add x delta) e' in
+        Forall (x, tau)
+      | Typeapp (e', tau) ->
+        if not (ok tau delta) then raise (UnboundTypeVarException "Unbound type variable.")
+        else
+          try
+            let Forall (x, tau') = type_check gamma delta e' in
+            type_subst tau' x tau
+          with Match_failure ->
+            raise (TypeApplicationException "Type application failure.") ;;
   end
 
 and Expr : Grammar.Expr
@@ -123,8 +204,10 @@ and Expr : Grammar.Expr
     | Lam of (string * tp) * expr
     (* e1 e2 *)
     | App of expr * expr
-    (* let x = e1 in e2 *)
-    | Let of string * expr * expr =
+    (* let x : tau = e1 in e2 *)
+    | Let of (string * tp) * expr * expr
+    | Typelam of string * expr
+    | Typeapp of expr * tp =
   struct
 
   end
