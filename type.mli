21a2de770bce44b357c9a809f88ae5b31f9a6bd8
Thomas Brooks <thomasbrooks@Thomass-MBP-6.lan>
2020-04-10 11:49:22 -0400

    tidy up Type signature, implement some typing methods

--- type.mli
+++ type.mli
@@ -2,53 +2,41 @@
   Type system for Spark
 *)
 
+module type Tp =
+sig
+  type gammaset
+  type deltaset
 
-type tp =
-  | Int
-  | Bool
-  | String
-  (* {l1 : tp1, ..., ln : tpn} *)
-  | Record of (string * tp) list
-  (* f : tp1 -> tp2 *)
-  | To of tp * tp
-  (* left/right-injection sum type *)
-  | Sum of tp * tp
-  (* universal type for parametric polymorphism *)
-  | Forall of string * tp
-  (* type variable for parametric polymorphism*)
-  | Typevar of string ;;
-
-
-module Gamma =
-  struct
-    type t = string * tp
-    let compare a b = String.compare (fst a) (fst b)
-  end ;;
+  type tp =
+    | Int
+    | Bool
+    | String
+    (* {l1 : tp1, ..., ln : tpn} *)
+    | Record of (string * tp) list
+    (* f : tp1 -> tp2 *)
+    | To of tp * tp
+    (* left/right-injection sum type *)
+    | Sum of tp * tp
+    (* universal type for parametric polymorphism *)
+    | Forall of string * tp
+    (* type variable for parametric polymorphism*)
+    | Typevar of string
 
 
-module Delta =
-  struct
-    type t = string
-    let compare = String.compare
-  end ;;
+  (*
+    ok : Method to check if a type variable is bound in a set.
+  *)
+  val ok : deltaset -> string -> bool
 
+  (*
+    new_type_var : Method to generate a new type variable distinct from every
+      element in a set.
+  *)
+  val new_type_var : deltaset -> string
 
-
-(*
-  GammaSet : A typing environment. Maps bound program variables to their types.
-    Implemented as a set of string * tp tuples.
-*)
-module GammaSet = Set.Make(Gamma) ;;
-type gammaset = GammaSet.t ;;
-
-(*
-  DeltaSet : A set of bound type variables.
-*)
-module DeltaSet = Set.Make(Delta) ;;
-type deltaset = DeltaSet.t ;;
-
-module StringSet = Set.Make(String) ;;
-type stringset = StringSet.t ;;
-
-
-val new_type_var :
+  (*
+    type_expr : Method to type an expression in given environment with a set of
+      bound type variables.
+  *)
+  val type_expr : gammaset -> deltaset -> Expr.expr -> tp
+end
